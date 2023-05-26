5c224b78f7d9abd9646cd082e099e0837ec4bc46
Akshay Chandel <115414944+Akshay-136@users.noreply.github.com>
2023-05-26 17:03:30 +0530

    Update 001. Two Sum.cpp

--- 001. Two Sum.cpp
+++ 001. Two Sum.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int, int>m;
         for(int i = 0; i < nums.size(); i++){
             if(m.count(target - nums[i])) return {m[target - nums[i]], i};
-            m[nums[i]] = i;
+            m[nums[i]] = i;  
         }
     }
 };  
